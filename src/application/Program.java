1bd9c4fa142ceffb6ffa05a9afa7c42607954668
FabianoCarregas <fabianofac_@hotmail.com>
2021-06-25 09:31:40 -0300

    ches layer and print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,28 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(3, 5);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
-		System.out.println(position);
-		
-		
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
-
+	
+	
+	
 }
---
+++ src/chess/ChassPiece.java
@@ -0,0 +1,24 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChassPiece extends Piece {
+	
+	private Color color;
+
+	public ChassPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+		
+	}
+	
+	
+
+
